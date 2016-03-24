5c0c9c969fd84644d788ef735e00df5edce7dbe1
Mokriy Yury <mavrand@mail.ru>
2016-03-24 16:16:44 +0300

    Add advanced player and gstreamer video modes

---
+++ ClientPublisher/advanced_player.py
@@ -0,0 +1,133 @@
+import gi, gstvideomode
+gi.require_version('Gtk', '3.0')
+gi.require_version('Gst', '1.0')
+from gi.repository import Gtk, Gst, GObject
+
+class PlayerWindow(Gtk.Window):
+    def __init__(self, player_settings):
+        self.build_window(player_settings["title"])
+        self.player_settings = player_settings
+
+    def build_window(self, title):
+        Gtk.Window.__init__(self, title=title)
+        self.main_box = Gtk.VBox()
+        self.add(self.main_box)
+        
+        self.top_hbox = Gtk.HBox()
+        self.main_box.pack_start(self.top_hbox, True, True, 0)
+
+        self.bottom_hbox = Gtk.HBox()
+        self.main_box.pack_start(self.bottom_hbox, True, True, 0)
+
+        self.radio_button_one_src = \
+            Gtk.RadioButton.new_with_label_from_widget(None, "One Source Mode")
+        self.radio_button_one_src.connect("toggled", self.on_button_toggled, "1")
+        self.top_hbox.pack_start(self.radio_button_one_src, True, True, 0)
+
+        self.radio_button_two_src = \
+            Gtk.RadioButton.new_with_label_from_widget(self.radio_button_one_src, "Two Source Mode")
+        self.radio_button_two_src.connect("toggled", self.on_button_toggled, "2")
+        self.top_hbox.pack_start(self.radio_button_two_src, True, True, 0)
+        
+        self.url_entry = Gtk.Entry()
+        self.bottom_hbox.pack_start(self.url_entry, True, True, 0)
+
+        self.state_button = Gtk.Button("Start")
+        self.bottom_hbox.pack_start(self.state_button, True, True, 0)
+        self.state_button.connect("clicked", self.start_stop)
+
+        self.connect("destroy", Gtk.main_quit)
+
+    def on_button_toggled(self, button, name):
+        if button.get_active():
+            if name == "1":
+                mode = gstvideomode.single_mode
+            elif name == "2":
+                mode = gstvideomode.first_in_second_mode
+            self.player_settings["video_mode"] = mode
+                                                  
+    def build_player(self, player_settings):
+        #Creating elements
+        self.create_elements(player_settings)
+
+        #Setting properties
+        self.set_properties(player_settings)
+
+        #Adding elements to player
+        self.add_elements(player_settings)
+
+        #Linking elements
+        self.link_elements(player_settings)
+
+        bus = self.player.get_bus()
+        bus.add_signal_watch()
+        bus.connect("message", self.on_message)
+
+    def create_elements(self, player_settings):
+        self.player = Gst.Pipeline.new("player")
+        self.rtmpsink = Gst.ElementFactory.make("rtmpsink", "rtmpsink")
+        self.muxer = Gst.ElementFactory.make("flvmux", "muxer")
+        self.muxer.set_property("streamable", True)
+        self.video_enc = Gst.ElementFactory.make(player_settings["video_enc"], "video_enc")
+        self.enc_filter = Gst.ElementFactory.make("capsfilter", "enc_filter")
+        self.video_parse = Gst.ElementFactory.make(player_settings["video_parse"], "video_parse")
+
+    def set_properties(self, player_settings):
+        enc_caps = Gst.Caps.from_string(player_settings["enc_caps"])
+        self.enc_filter.set_property("caps", enc_caps)
+        for param, value in player_settings["video_enc_properties"]:
+            self.video_enc.set_property(param, value)
+
+    def add_elements(self, player_settings):
+        self.video_mixer = player_settings["video_mode"](self.player, player_settings)
+        self.player.add(self.video_enc)
+        self.player.add(self.enc_filter)
+        self.player.add(self.muxer)
+        self.player.add(self.rtmpsink)
+
+    def link_elements(self, player_settings):
+        self.video_mixer.link(self.video_enc)
+        self.video_enc.link(self.enc_filter)
+        self.enc_filter.link(self.muxer)
+        self.muxer.link(self.rtmpsink)
+        
+    def start_stop(self, w):
+        if self.state_button.get_label() == "Start":
+            self.build_player(self.player_settings)
+            urlpath = self.url_entry.get_text().strip()
+            self.state_button.set_label("Stop")
+            self.player.get_by_name("rtmpsink").set_property("location", urlpath)
+            self.player.set_state(Gst.State.PLAYING)
+        else:
+            self.player.set_state(Gst.State.NULL)
+            self.state_button.set_label("Start")
+
+    def on_message(self, bus, message):
+        t = message.type
+        if t == Gst.MessageType.EOS:
+            self.player.set_state(Gst.State.NULL)
+            self.state_button.set_label("Start")
+        elif t == Gst.MessageType.ERROR:
+            self.player.set_state(Gst.State.NULL)
+            self.state_button.set_label("Start")
+            err, debug = message.parse_error()
+            print("Error: %s" % err, debug)
+        
+default_settings = {
+    "title" : "Player",
+    "video_mode" : gstvideomode.first_in_second_mode,
+    "videosrc" : ("v4l2src", "ximagesrc"),
+    "videosrc_properties" : ((), (("use-damage", 0),)),
+    "video_caps" : ("video/x-raw,width=320,height=240", None),
+    "video_enc" : "x264enc",
+    "video_enc_properties" : (("tune", "zerolatency"),), 
+    "enc_caps" : "video/x-h264,profile=baseline",
+    "video_parse" : "h264parse",
+} 
+
+if __name__ == "__main__":
+    Gst.init(None)
+    win = PlayerWindow(default_settings)
+    win.connect("delete-event", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
---
+++ ClientPublisher/gstvideomode.py
@@ -0,0 +1,68 @@
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst
+
+
+def create_element(name, element):
+    return Gst.ElementFactory.make(element, name)
+
+
+def set_properties(videosrc, properties):
+    for prop in properties:
+        videosrc.set_property(prop[0], prop[1])
+
+def add_elements(player, elements):
+    for elem in elements:
+        if elem != None:
+            player.add(elem)
+        
+def create_filter(name, video_caps):
+    print(video_caps)
+    if video_caps == None:
+        return None
+    caps = Gst.Caps.from_string(video_caps)
+    video_filter = create_element(name, "capsfilter")
+    video_filter.set_property("caps", caps)
+    return video_filter
+
+def link_elements(src, sink):
+    if src != None and sink != None:
+        assert src.link(sink)
+
+def single_mode(player, player_settings):
+    videosrc = create_element("videosrc0", player_settings["videosrc"][0])
+    set_properties(videosrc, player_settings["videosrc_properties"][0])
+    video_filter = create_filter("video_caps0", player_settings["video_caps"][0])
+    video_mixer = create_element("video_mixer", "videomixer")
+
+    add_elements(player, (videosrc, video_filter, video_mixer))
+    
+    videosrc.link(video_filter)
+    video_filter.link(video_mixer)
+    return video_mixer
+    
+
+def first_in_second_mode(player, player_settings):
+    videosrcs = tuple(create_element("videosrc%s" % i, player_settings["videosrc"][i])
+                     for i in range(2))
+    for i in videosrcs:
+        print(i)
+    video_filters = tuple(create_filter("video_filter%s" % i, player_settings["video_caps"][i])
+                          for i in range(2))    
+    video_mixer = create_element("video_mixer", "videomixer")
+    video_box = create_element("video_box", "videobox")
+
+    set_properties(video_box, (("border-alpha", 0), ("alpha", 0.5), ("left", -10), ("top", -10)))
+    for i in range(2):
+        set_properties(videosrcs[i], player_settings["videosrc_properties"][i])
+    
+    add_elements(player, videosrcs + video_filters + (video_box, video_mixer))
+    for i in range(2):
+        link_elements(videosrcs[i], video_filters[i])
+    link_elements(video_filters[0] if video_filters[0] != None else videosrcs[0], video_box)
+    link_elements(video_filters[1] if video_filters[1] != None else videosrcs[1], video_mixer)
+    link_elements(video_box, video_mixer)
+    return video_mixer
+    
+    
+    
